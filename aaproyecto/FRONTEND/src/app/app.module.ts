cfd60b8da362e8d49f343a35921e8ea76f574797
juanjolanza <jlanzavechia@gmail.com>
2023-06-14 11:16:29 -0300

    Antes de cambiar versión de spring boot a 2.7.7
    Antes de cambiar versión de spring boot a 2.7.7

--- aaproyecto/FRONTEND/src/app/app.module.ts
+++ aaproyecto/FRONTEND/src/app/app.module.ts
@@ -14,7 +14,23 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'; 
+import { RouterModule, Routes } from '@angular/router';
 
+//PROBANDO RUTEAR GUIADO DE UN VIDEO DE YOUTUBE DE PÍLDORAS INFOORMÁTICAS
+
+const appRoutes: Routes= [
+{path:'', component:HomeComponent}, 
+{path:'login',component:LoginComponent},
+{path:'educacion',component:EducacionComponent},
+{path:'experiencia',component:ExperienciaComponent},
+{path:'acerca-de',component:AcercaDeComponent}
+
+]
+
+//HASTA ACÁ, LO DE PÍLDORAS INFORMÁTICAS
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,12 +43,19 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({}) 
+    NgCircleProgressModule.forRoot({}), 
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes) 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- aaproyecto/FRONTEND/src/app/components/logo-ar/logo-ar.component.ts
+++ aaproyecto/FRONTEND/src/app/components/logo-ar/logo-ar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-ar',
   templateUrl: './logo-ar.component.html',
   styleUrls: ['./logo-ar.component.css']
 })
-export class LogoArComponent {
+export class LogoArComponent implements OnInit{
 
+  constructor(private router:Router){}
+
+  ngOnInit():void{
+  }
+
+  login() {
+    this.router.navigate(['/login'])  
+  }
 }
